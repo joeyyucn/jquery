396c00febd910afc60670899fe4fb7823739400b
joeyyucn <373771454@qq.com>
2016-03-04 17:05:45 +0800

    update page3.html

--- js/page3.js
+++ js/page3.js
@@ -6,19 +6,56 @@ function showAllCatetories() {
 }
 
 function showSimplifiedCategories() {
-    $("li:gt(7)").hide();
+    $("li:gt(5):not(:last)").hide();
     $(".showmore > a > span").html("show more");
 }
 
 $(document).ready(function () {
-    showSimplifiedCategories();
-    $(".showmore > a > span").click(function () {
-        if (visible) {
-            showSimplifiedCategories();
+    
+    var $categories = $(".SubCategoryBox > ul > li:gt(5):not(:last)");
+    $categories.hide();
+    $("div.showmore > a").click(function () {
+        
+        if ($categories.is(":visible")) {
+            $categories.hide(200);
+            $(".showmore > a > span").text("Show More");
         }
         else {
-            showAllCatetories();
+            $categories.show(200);
+            $(".showmore > a > span").text("Show Less");
         }
-        visible = !visible;
+        return false;
+    });
+    var $watermelon = $('<li title="watermelon">watermelon</li>');
+    $watermelon.click(function () {
+        alert($(this).attr("class"));
+
+    });
+
+    $("#append").click(function () {
+        $("#fruits").append($watermelon.clone(true));
+    });
+
+    $("#prepend").click(function () {
+        $("#fruits").prepend($('<li title="watermelon">watermelon</li>'));
+    });
+    $("#clear").click(function () {
+        $("#fruits li[title=watermelon]").remove();
+    });
+
+    $("#next").click(function () {
+        $("p").replaceWith("<strong>which one you dislike</strong>");
+    });
+
+    $("#wrap").click(function () {
+        $("strong").wrap("<p></p>");
+    });
+
+    $("#wrap").click(function () {
+        $("strong").wrapAll("<a></a>");
+    });
+
+    $("#removetitle").click(function () {
+        $("li").removeAttr("title");
     });
 });
